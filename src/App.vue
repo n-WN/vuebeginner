e690e665594cc8fa614fd1d7ba6318a7da800104
n-WN <T@>
2023-12-28 17:31:32 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<script>
+import ifgrammar from './components/ifgrammar.vue'
+export default {
+  data() {
+    return {
+      msg:"牛魔",
+      dynamicClass: "testClass",
+      objectOfAttrs: {
+        class: "dynamicClass",
+        id: "appid"
+      },
+    }
+  }
+}
+</script>
+
+<template>
+<!--  <h3>template</h3>-->
+<!--  模版插值-->
+<!--  动态绑定-->
+<!--  <div v-bind:class="dynamicClass">{{ msg }}</div>-->
+<!--  对象绑定-->
+<!--  <div v-bind="objectOfAttrs">object</div>-->
+<!--  if-->
+<ifgrammar/>
+</template>
+
+<style>
+.testClass{
+  color: red;
+}
+.dynamicClass{
+  color: darkgreen;
+  font-size: 20px;
+}
+</style>
---
+++ src/components/ifgrammar.vue
@@ -0,0 +1,7 @@
+<script>
+
+</script>
+
+<template>
+ <h3>条件渲染</h3>
+</template>
